ee4750d2aa0b542c8a03e31695943ef1dbee1a73
syaswanthkumar66 <syaswantkumar66@gmail.com>
2026-05-05 22:09:41 +0530

    revert: remove mobile-app from tracking to prevent deployment errors

--- mobile-app/src/config/env.js
+++
@@ -1,6 +0,0 @@
-export const API_BASE_URL = 'https://iot-8nyv.onrender.com/api/v1';
-
-export const DEVICE_WARNING_MS = 8 * 1000;
-export const DEVICE_STALE_MS = 15 * 1000;
-export const DEVICE_STALE_CHECK_MS = 5 * 1000;
-export const MQTT_KEEPALIVE_SECONDS = 30;
